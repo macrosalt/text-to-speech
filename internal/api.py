e9bcb5fa6e807093a968b01c04d4efd847812b99
Yan Li <salt.liyan@shopee.com>
2023-04-16 20:32:22 +0800

    feat: add va

--- internal/api.py
+++ internal/api.py
@@ -1,24 +1,10 @@
-import requests, base64
-
-myobj = {
-  "audioConfig": {
-    "audioEncoding": "OGG_OPUS",
-    "effectsProfileId": [
-      "small-bluetooth-speaker-class-device"
-    ],
-    "pitch": 0,
-    "speakingRate": 1
-  },
-  "input": {
-    "text": "hello world"
-  },
-  "voice": {
-    "languageCode": "en-US",
-    "name": "en-US-Neural2-C"
-  }
-}
+import requests, base64, voice_actor
 
 def send_request(url, input):
+    myobj = voice_actor.myobj
+    # use self define voice
+    # name, clean_input = func(input)
+    # myobj = voice_actor.get_voice_actor(name)
     myobj["input"] = {"text": input}
     x = requests.post(url, json = myobj)
     resp = x.json()
---
+++ internal/voice_actor.py
@@ -0,0 +1,49 @@
+import copy
+
+myobj = {
+  "audioConfig": {
+    "audioEncoding": "OGG_OPUS",
+    "effectsProfileId": [
+      "small-bluetooth-speaker-class-device"
+    ],
+    "pitch": 0,
+    "speakingRate": 1
+  },
+  "input": {
+    "text": "hello world"
+  },
+  "voice": {
+    "languageCode": "en-US",
+    "name": "en-US-Neural2-C"
+  }
+}
+
+voice_actor_list = [
+    "en-US-Neural2-A",
+    "en-US-Neural2-C",
+    "en-US-Neural2-D",
+    "en-US-Neural2-E",
+    "en-US-Neural2-F",
+    "en-US-Neural2-G",
+    "en-US-Neural2-H",
+    "en-US-Neural2-I",
+    "en-US-Neural2-J"
+]
+
+cur_voice_actor_index = 0
+voice_actor_map = {}
+
+def get_voice_actor(name):
+    global myobj
+    global voice_actor_list
+    global cur_voice_actor_index
+    global voice_actor_map
+    
+    res = copy.deepcopy(myobj)
+    if name not in voice_actor_map:
+        # get in round robin
+        voice_actor_map[name] = voice_actor_list[cur_voice_actor_index]
+        cur_voice_actor_index = (cur_voice_actor_index + 1) % len(voice_actor_list)
+    
+    res["voice"]["name"] = voice_actor_map[name]
+    return res
