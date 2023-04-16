e939c3a888513d22d9587194850603fe8710d02a
Yan Li <salt.liyan@shopee.com>
2023-04-16 21:01:02 +0800

    feat: add one self defined parse

--- internal/api.py
+++ internal/api.py
@@ -3,10 +3,15 @@ import requests, base64, voice_actor
 def send_request(url, input):
     myobj = voice_actor.myobj
     # use self define voice
-    # name, clean_input = func(input)
+    # name, input = get_va_name_and_txt(input)
     # myobj = voice_actor.get_voice_actor(name)
     myobj["input"] = {"text": input}
     x = requests.post(url, json = myobj)
     resp = x.json()
     return base64.b64decode(resp["audioContent"])
 
+def get_va_name_and_txt(input):
+    splits = input.split(":", 1)
+    if len(splits) == 1:
+        return "default", splits[0]
+    return splits[0], splits[1]
